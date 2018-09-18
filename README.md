368e96ba7288e1560892a04a422412f3610548de
Hlynur Magnus <futomatik@gmail.com>
2018-09-18 09:19:10 +0000

    Fyrsta commit med einfoldum utskyringum a uppsetningu

---
+++ README.md
@@ -0,0 +1,1 @@
+Tile traveller is a little dungeon crawler game where walls are either open or closed
---
+++ tileTraveller.py
@@ -0,0 +1,13 @@
+# Hlynur Magnus Magnusson
+# Hlynurm18@ru.is
+# Assignment 8:  tileTraveller
+# https://github.com/HlynurM/tileTraveller.git
+
+
+# Setja upp fall sem vinnur ur key input
+# Fall sem tekur key input og setur inn i mover(x,y)
+# Fall sem athugar hvort veggir seu til stadar
+# Fall sem prentar ut villuskilabod ef leid er lokud 
+#               print("Not a valid direction")
+# Setja upp fall sem init_fall() sem setur upp hluti i byrjun
+# Fall sem keyrir adal luppu start() fall
