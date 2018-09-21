ab3a6cdaaa41c11f72f4840963815d5741ae6b96
Hlynur Magnus <futomatik@gmail.com>
2018-09-21 14:00:56 +0000

    Finally a proper working Function version!

--- tileFunctions02.py
+++ tileFunctions02.py
@@ -3,15 +3,14 @@
 # Assignment 8:  tileTraveller
 # https://github.com/HlynurM/tileTraveller.git
 
-# FASTAR
-
 # BYRJUNAR GILDI
 pos_x, pos_y = 1, 1
+n, s, e, w = 1, -1, 1, -1 
 
-# Athuga hvort styttri leid se ad innleida current_tile. Hvort loopa eda auka fall inni 
-# fallinu virki betur. Eda ekki!
+# Tekur inn x, y og skilar ut mogulegar leidir
 def current_tile(x_hnit, y_hnit):
-    """Skilar ut mogulegar attir midad vid nuverandi hnit; compass, possible moves"""
+    """Skilar ut mogulegar attir midad vid nuverandi hnit; 
+    direction_map, possible_input_key"""
     victory = False
     if x_hnit == 1 and y_hnit == 1:     # 1,1
         direction_map = "(N)orth."
@@ -32,59 +31,58 @@ def current_tile(x_hnit, y_hnit):
         direction_map = "(E)ast or (W)est."
         possible_input_key = 'ew'
     elif x_hnit == 3 and y_hnit == 1:   # 3,1 Victory
-        victory = True
         direction_map = "(N)orth."
         possible_input_key = 'n'
+        victory = True
     elif x_hnit == 3 and y_hnit == 2:   # 3,2
         direction_map = "(N)orth or (S)outh."
         possible_input_key = 'ns'
     elif x_hnit == 3 and y_hnit == 3:   # 3,3
         direction_map = "(S)outh or (W)est."
         possible_input_key = 'sw'
-    else:
-        dir_error()
     return direction_map, possible_input_key, victory
 
 def dir_error():
+    """Calls out invalid moves"""
     print("Not a valid direction!")
 
-def mover(my_dir, x, y, valid_direction):
-    if ((my_dir == 'n') and ('n' in valid_direction)):    
-        y += 1
-    elif ((my_dir == 's') and ('s' in valid_direction)):
-        y -= 1
-    elif ((my_dir == 'e') and ('e' in valid_direction)):
-        x += 1
-    elif ((my_dir == 'w') and ('w' in valid_direction)):
-        x -= 1
+def mover(my_dir, valid_direction, x, y):
+    """Moves player. Takes in: 
+    Key input, valid directions and current x and y position"""
+    if (my_dir in valid_direction):
+        if my_dir == 'n' or my_dir == 's':
+            y += eval(my_dir)
+        else:
+            x += eval(my_dir)
     else:
-        print("Not a valid direction!")
+        # If move not valid, call out the error!
+        dir_error()
     return x, y
 
 def input_key():
+    """Returns keystroke input: lower case"""
     direction = input("Direction: ").lower()
     return direction
 
-# Starta leik
+def travel_guide(map, victory):
+    """Prints out possible directions from current tile"""
+    if victory != True:
+        print("You can travel: " + map)
+    else:
+        print("Victory!")
+
+# Start the dungeon crawler!
 while 1:
+    # What tile is player on currently
     direction_map, possible_key_input, victory = current_tile(pos_x, pos_y)
-    print("Map:", direction_map, "key:", possible_key_input, "victory", victory)
 
-    print("You can travel: " + direction_map)
-    the_key = input_key()
-    mover(the_key, pos_x, pos_y, possible_key_input)
+    # Where can I travel
+    travel_guide(direction_map, victory)
+    if victory == True:
+        break
 
+    # What is my direction choice
+    direction = input_key()
 
-    # # Debug /////////////////////////////////////////////////////////////////////////////////////////////
-    # print("Compass: ", direction_map, "Valid path key: ", possible_key_input, "Victory:" ,victory)
-
-    # if victory == False:
-    #     print("Victory!")
-    #     break
-
-    where_am_i(x, y)            # 3 x 3 tile dungeon
-
-    tell_me_open_paths(x, y)    # N S E W
-
-    move_me()                   # update x, y based on open N S E W
-
+    # Update x, y coordinates to move character based on possible moves.
+    pos_x, pos_y = mover(direction, possible_key_input, pos_x, pos_y)
